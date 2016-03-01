d01fde54cfae8b6fd0417bffd6dbb53e2ba6e097
Rod c <raadce@gmail.com>
2016-03-01 17:49:00 +0000

    creating article association with user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,8 +10,13 @@ class ArticlesController < ApplicationController
     end
     def create
     @article = Article.new(article_params)
-    @article.save
+    @article.user = User.first
+    if @article.save
+    flash[:success] = "Article was successfully created"
     redirect_to article_path(@article)
+    else
+    render 'new'
+    end
     end
     
     def show 
